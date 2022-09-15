88ef6f1b484099f0cfbdcbd8551ebdbf89362262
Floki <a5ap.floki@gmail.com>
2022-09-15 14:36:03 +0100

    main

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
    <BrowserRouter>
    <Navbar />
       <Routes>
-       <Route path="about/*" element={<About/>} />
+       <Route path="/" element={<About/>} />
         <Route path="projects/*" element={<Projects/>} />
         <Route path="contact/*" element={<Contact/>} />
         <Route path="skills/*" element={<Skills/>} />
